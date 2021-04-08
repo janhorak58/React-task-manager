5e1e773b29dfc42d2a1bcae5039f58c0a431e4b9
janhorak58 <janhorak58@gmail.com>
2021-04-08 09:38:12 +0200

    Default Commit

--- src/index.js
+++ src/index.js
@@ -3,15 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Footer from './components/Footer'
+import Header from './components/Header'
 
 ReactDOM.render(
   <React.StrictMode>
+    <Header></Header>
     <App />
+    <Footer></Footer>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
